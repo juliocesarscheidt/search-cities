9ff3a76bc1703304315a6a5591a852ceff70037e
Julio Cesar <julioscheidt@hotmail.com>
2019-01-28 01:04:38 -0200

    v1.1.1
    Signed-off-by: Julio Cesar <julioscheidt@hotmail.com>

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,31 +1,20 @@
 <template>
-  <div class="container d-flex align-content-center align-items-center flex-column">
+  <div class="wrapper d-flex align-content-center align-items-center flex-column" v-bind:class="{ black: applyClass }">
     <hr>
     <div class="form-group mt-2">
       <label for="selectedState">State</label>
-      <select id="selectedState" v-model="selectedState" name="selectedState" class="form-control" v-on:change="onChangeState">
+      <select id="selectedState" v-model="selectedState" name="selectedState" class="form-control" v-bind:class="{ black: applyClass }" v-on:change="onChangeState">
         <option v-for="state in states" v-bind:key="state.id" v-bind:value="state.id">{{ state.name }}</option>
       </select>
     </div>
     <div class="form-group mt-2">
       <label for="selectedCity">City</label>
-      <select id="selectedCity" v-model="selectedCity" name="selectedCity" class="form-control" v-on:change="onChangeCity">
+      <select id="selectedCity" v-model="selectedCity" name="selectedCity" class="form-control" v-bind:class="{ black: applyClass }" v-on:change="onChangeCity">
         <option v-for="city in cities" v-bind:key="city.id" v-bind:value="city.id">{{ city.name }}</option>
       </select>
     </div>
     <hr>
-    <!-- <div class="form-group mt-2">
-      <label for="multiselect-state">State</label>
-      <multiselect
-        v-model="selectedState" :options="states" name="multiselect-state" label="name" :searchable="true" :allow-empty="false" track-by="name" v-on:change="onChangeState"
-      ></multiselect>
-    </div>
-    <div class="form-group mt-2">
-       <label for="multiselect-city">City</label>
-      <multiselect
-        v-model="selectedCity" :options="cities" name="multiselect-city" label="name" :searchable="true" :allow-empty="false" track-by="name" v-on:change="onChangeCity"
-      ></multiselect>
-    </div> -->
+    <button class="btn btn-large btn-primary mt-5" v-on:click="setColor">Mudar Cor <i class="fas fa-adjust"></i></button>
   </div>
 </template>
 
@@ -41,7 +30,8 @@ export default {
       ],
       states: [],
       selectedState: 0,
-      selectedCity: 0
+      selectedCity: 0,
+      applyClass: false
     };
   },
   mounted () {
@@ -50,15 +40,18 @@ export default {
         let _states = res.data.states;
         _states.unshift({ 'id': 0, 'name': '-- Selecione --' });
         this.states = _states;
-
         this.selectedState = this.getFromStorage('user_states') || 0;
-
         if (this.selectedState) {
           // getting correlated cities information
           this.getCities(this.selectedState);
           this.selectedCity = this.getFromStorage('user_cities');
         }
       });
+    if (this.getFromStorage('user_color_applied')) {
+      this.applyClass = this.getFromStorage('user_color_applied') === 'true' ? true : false;
+    } else {
+      this.setStorage('user_color_applied', this.applyClass);
+    }
   },
   methods: {
     setStorage (propStorage, thisAttr) {
@@ -73,6 +66,10 @@ export default {
         let _selected = localStorage.getItem(propStorage);
         return _selected || 0;
       };
+    },
+    setColor () {
+      this.applyClass = !this.applyClass;
+      this.setStorage('user_color_applied', this.applyClass);
     },
     getCities (selectedState) {
       getCities(selectedState)
@@ -89,7 +86,6 @@ export default {
       } else {
         this.setStorage('user_states', 0);
       }
-
       this.selectedCity = 0;
       this.setStorage('user_cities', this.selectedCity);
     },
@@ -99,7 +95,6 @@ export default {
       }
     }
   }
-
 };
 </script>
 
