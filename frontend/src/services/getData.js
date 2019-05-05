5c47d5b72c2bcde6f7f0a7672dfdd45beb98ff3f
Julio Cesar <julioscheidt@hotmail.com>
2019-05-05 19:17:13 -0300

    Changes in services
    Signed-off-by: Julio Cesar <julioscheidt@hotmail.com>

--- frontend/src/services/getData.js
+++ frontend/src/services/getData.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const urlBase = process.env.API_HOST || `//blackdevs.com.br:9000`;
-// const urlBase = process.env.API_HOST || `//${window.location.hostname}:9000`;
+const urlBase = process.env.API_HOST || `https://blackdevs.com.br:9000`;
 
 const getStates = () => {
   return axios.get(`${urlBase}/api/v1/states`);
