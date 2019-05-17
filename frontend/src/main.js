85793a7118c1cd0fcc6b7b5586390d62f416fd5a
Julio Cesar <julioscheidt@hotmail.com>
2019-05-17 15:01:49 -0300

    Add Sentry integration
    Signed-off-by: Julio Cesar <julioscheidt@hotmail.com>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import Multiselect from 'vue-multiselect';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
@@ -14,4 +16,14 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
+});
+
+Sentry.init({
+  dsn: 'https://a1b9ab836d9a4270a5a770855fb6c4fb@sentry.io/1462253',
+  integrations: [
+    new Integrations.Vue({
+      Vue,
+      attachProps: true
+    })
+  ]
 });
