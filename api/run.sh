d4a07af7874625721872a8526744ff67723ee946
Julio Cesar <julioscheidt@hotmail.com>
2019-07-01 22:16:58 -0300

    Version route and changes in containers
    Signed-off-by: Julio Cesar <julioscheidt@hotmail.com>

--- api/run.sh
+++ api/run.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 NPM_CMD=$(which npm)
-# export SHA="$(git rev-parse HEAD)"
-# cat ./.env | sed -r "s/SHA=.*/SHA=$SHA\n/gim"
-# SHA=${SHA:0:7}
-
 echo "$SHA"
-
 sleep 15
 
 VERSION=$SHA && "$NPM_CMD" run dev
